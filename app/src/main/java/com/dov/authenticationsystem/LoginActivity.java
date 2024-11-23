2c624f353297e8ab820023de2fb3e9a386c77e1e
dovene <adovene@gmail.com>
2024-11-23 19:04:21 +0100

    rename layout login

--- app/src/main/java/com/dov/authenticationsystem/LoginActivity.java
+++ app/src/main/java/com/dov/authenticationsystem/LoginActivity.java
@@ -15,7 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
         loginEditText = findViewById(R.id.login_edit);
